11e358ef68b56fad7645ae6bfaca91e1a94220b7
Adam Streck <adam.streck@gmail.com>
2012-11-24 22:21:33 +0100

    Results commented.

--- src/com/gearfish/medicalscanner/Results.java
+++ src/com/gearfish/medicalscanner/Results.java
@@ -11,11 +11,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * This class finds the data content based on the key either provided from the
+ * database or from the result screen. Display the data and based on their
+ * nature set the bottom picture.
+ * 
+ * @author punyone
+ * 
+ */
 public class Results extends Activity {
     SharedPreferences prefs;
 
-    TextView result_text;
-
+    /**
+     * Determines which picture should be displayed based on the output.
+     * 
+     * @author punyone
+     * 
+     */
     enum Pic_type {
 	human, enemy, error
     };
@@ -29,11 +41,15 @@ public class Results extends Activity {
 	setContentView(R.layout.results);
 	prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-	// Get the result picked in database or use the last one if none was
-	// picked
+	// Get the data:
+	// Find out what the last result was - use 0 if you can't find it.
 	int result_num = prefs.getInt(getString(R.string.results_number), 0);
+	// Get a result number if it was scanned (as opposed to invoked from
+	// database).
 	result_num = getIntent().getIntExtra(getString(R.string.result_choice), result_num);
+	// Get key for the results itself.
 	String key = prefs.getString(Integer.toString(result_num), getString(R.string.err_result));
+	// Get the keyed data from the XML file.
 	String value = getData(key);
 
 	// Control presence of the "nepritel" string
@@ -44,7 +60,7 @@ public class Results extends Activity {
 	    my_pic = Pic_type.error;
 
 	// Display the result text
-	result_text = (TextView) findViewById(R.id.resultText);
+	TextView result_text = (TextView) findViewById(R.id.resultText);
 	result_text.setText(value);
     }
 
@@ -52,6 +68,8 @@ public class Results extends Activity {
     public void onResume() {
 	super.onResume();
 
+	// Set the bottom picture based on the content of the result - also, if
+	// there is no more battery, return from this activity.
 	if (Battery.setActivity(this)) {
 	    if (my_pic == Pic_type.human)
 		((ImageView) findViewById(R.id.resultLogo)).setImageResource(R.drawable.results2a);
@@ -64,6 +82,14 @@ public class Results extends Activity {
 	}
     }
 
+    /**
+     * This function reads key value from the database. Error string is set if
+     * the key is not found or if the text is empty.
+     * 
+     * @param key
+     *            string that is searched for
+     * @return the content of the key if it is found, otherwise an error string
+     */
     String getData(final String key) {
 	String value = getString(R.string.err_data);
 
@@ -74,26 +100,31 @@ public class Results extends Activity {
 	else
 	    xpp = getResources().getXml(R.xml.pre_calib);
 
-	
 	try {
+	    // Go through all the tags.
 	    for (; xpp.getEventType() != XmlPullParser.END_DOCUMENT; xpp.next()) {
+		// Wait for starting tag.
 		if (xpp.getEventType() != XmlPullParser.START_TAG)
 		    continue;
+		// Use only those that have an attribute.
 		if (xpp.getAttributeCount() == 0)
 		    continue;
+		// Wait for the one whose attribute matches the key.
 		else if (xpp.getAttributeValue(0).equals(key)) {
-		    while(xpp.getEventType() != XmlPullParser.TEXT && xpp.getEventType() != XmlPullParser.END_DOCUMENT)
+		    // Get the text node.
+		    while (xpp.getEventType() != XmlPullParser.TEXT && xpp.getEventType() != XmlPullParser.END_DOCUMENT)
 			xpp.next();
-		    
-		    value = xpp.getText();
+
+		    // Get the data or leave the error string if the data are
+		    // null.
+		    value = (xpp.getText() == null) ? value : xpp.getText();
 		}
 	    }
-	}
-	catch (Throwable t) {
+	} catch (Throwable t) {
 	    t.printStackTrace();
 	}
 
-	return (value != null ? value : "");
+	return value;
     }
 
     /**
--- src/com/gearfish/medicalscanner/Zbar.java
+++ src/com/gearfish/medicalscanner/Zbar.java
@@ -1,10 +1,14 @@
 package com.gearfish.medicalscanner;
 
 /**
- * This class is taken from the original project.
+ * This class provides a wrapper for launching Zbar library function that
+ * implements qr code recognition.
  * 
- * This class provides a wrapper for launching zbar library function
- * that implements qr code recognition
+ * This class is taken from the original project OBSQR by Anna Kruglik and Serge
+ * Zaitsev. See
+ * https://play.google.com/store/apps/details?id=trikita.obsqr&hl=en for
+ * details.
+ * 
  */
 class Zbar {
     static {
