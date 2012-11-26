e4b747e6f5b489cfee2a638614597f271ee33b08
Adam Streck <adam.streck@gmail.com>
2012-11-26 16:31:10 +0100

    Real battery now reflected in more precise manner.

--- src/com/gearfish/medicalscanner/Battery.java
+++ src/com/gearfish/medicalscanner/Battery.java
@@ -25,13 +25,15 @@ import android.widget.ImageView;
 class Battery extends BroadcastReceiver {
     private final static int SECOND = 1000;
     private final static int SECONDS_PER_MINUTE = 60;
-    private final static int STEPS = 100; // Lenght of the scale of battery values.
+    private final static int STEPS = 100; // Lenght of the scale of battery
+					  // values.
 
     private static int running_time = 1; // Runtime given in seconds.
     private static int remaining_time = 1; // What is remaining of the runtime.
 
-    private static int value = 0; // Current value of the battery on the scale
-				  // from 0 to 100.
+    private static int current_ratio = 0; // Current value of the battery on the scale
+				  // from 0 to 100. Initially on 100.
+    private static int real_battery = 100; // Real battery status as given by last update. Initially on 100.
 
     private static Activity current_act = null; // Activity that is currently
 						// active.
@@ -39,7 +41,7 @@ class Battery extends BroadcastReceiver {
      * Schedules updates.
      */
     static Timer minuter;
-    
+
     /**
      * A handler that updates the picture dependent on the current level.
      */
@@ -64,7 +66,7 @@ class Battery extends BroadcastReceiver {
 		minuter.cancel();
 
 	    // Update data.
-	    value = Math.min(value, computeValue(remaining_time,running_time));
+	    current_ratio = Math.min(real_battery, computeValue(remaining_time, running_time));
 	    handler.sendEmptyMessage(0);
 	}
     };
@@ -77,7 +79,7 @@ class Battery extends BroadcastReceiver {
      */
     public void setTime(int time_in_mins) {
 	running_time = remaining_time = time_in_mins * SECONDS_PER_MINUTE;
-	value = STEPS;
+	current_ratio = STEPS;
 	if (minuter != null)
 	    minuter.cancel();
 	minuter = new Timer();
@@ -85,11 +87,14 @@ class Battery extends BroadcastReceiver {
     }
 
     /**
-     * Computes ratio of current energy to the remaining one and ceils it so it ends really at the last moment.
+     * Computes ratio of current energy to the remaining one and ceils it so it
+     * ends really at the last moment.
      * 
-     * @param current	current energy value
-     * @param scale	scale of the energy value
-     * @return	current energy on the scale from 0 to 100
+     * @param current
+     *            current energy value
+     * @param scale
+     *            scale of the energy value
+     * @return current energy on the scale from 0 to 100
      */
     private int computeValue(int current, int scale) {
 	double curr = current;
@@ -98,19 +103,19 @@ class Battery extends BroadcastReceiver {
 	double value = Math.ceil(ratio * STEPS);
 	return (int) value;
     }
-    
+
     @Override
     public void onReceive(Context context, Intent intent) {
 	// Move the scale by 10% so it ends with 10% of energy if necessary.
 	int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-	int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) - (int) Math.round(scale * 0.1);
-	level = Math.max(0, level);
-	
+	int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+	level = Math.max(0, level - 10);
+
 	Log.e("BatteryManager", "level is " + level + "/" + scale);
 
 	// Give the remaining number of percent of battery as a min(runtime,real
 	// battery)
-	value = Math.min(value, computeValue(level, scale));
+	real_battery = computeValue(level, scale);
     }
 
     /**
@@ -140,8 +145,9 @@ class Battery extends BroadcastReceiver {
 	if (current_act == null)
 	    return false;
 
-	if (value <= 0) {
-	    // End the activity if it is not the main one. Otherwise set the final picture.
+	if (current_ratio <= 0) {
+	    // End the activity if it is not the main one. Otherwise set the
+	    // final picture.
 	    if (current_act.getClass() != MainActivity.class)
 		current_act.finish();
 	    else {
@@ -156,11 +162,11 @@ class Battery extends BroadcastReceiver {
 
 	    // Pick the picture.
 	    int picture_ID = 0;
-	    if (value >= 75)
+	    if (current_ratio >= 75)
 		picture_ID = R.drawable.battery_01;
-	    else if (value >= 50)
+	    else if (current_ratio >= 50)
 		picture_ID = R.drawable.battery_02;
-	    else if (value >= 25)
+	    else if (current_ratio >= 25)
 		picture_ID = R.drawable.battery_03;
 	    else
 		picture_ID = R.drawable.battery_04;
